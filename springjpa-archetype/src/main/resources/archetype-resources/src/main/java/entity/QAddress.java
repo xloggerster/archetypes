32d5d23cb4f913d963126c6ea1125bb9f0d9fa9a
Ralph Schaer <ralphschaer@gmail.com>
2011-12-27 15:02:53 +0100

    updated jetty, guava, apache poi

--- springjpa-archetype/src/main/resources/archetype-resources/src/main/java/entity/QAddress.java
+++ springjpa-archetype/src/main/resources/archetype-resources/src/main/java/entity/QAddress.java
@@ -3,16 +3,13 @@
 #set( $symbol_escape = '\' )
 package ${package}.entity;
 
-import static com.mysema.query.types.PathMetadataFactory.forVariable;
+import static com.mysema.query.types.PathMetadataFactory.*;
+
+import com.mysema.query.types.*;
+import com.mysema.query.types.path.*;
 
 import javax.annotation.Generated;
 
-import com.mysema.query.types.PathMetadata;
-import com.mysema.query.types.path.EntityPathBase;
-import com.mysema.query.types.path.EnumPath;
-import com.mysema.query.types.path.NumberPath;
-import com.mysema.query.types.path.PathInits;
-import com.mysema.query.types.path.StringPath;
 
 /**
  * QAddress is a Querydsl query type for Address
@@ -20,44 +17,44 @@ import com.mysema.query.types.path.StringPath;
 @Generated("com.mysema.query.codegen.EntitySerializer")
 public class QAddress extends EntityPathBase<Address> {
 
-	private static final long serialVersionUID = -2065239942;
+    private static final long serialVersionUID = -2065239942;
 
-	private static final PathInits INITS = PathInits.DIRECT;
+    private static final PathInits INITS = PathInits.DIRECT;
 
-	public static final QAddress address = new QAddress("address");
+    public static final QAddress address = new QAddress("address");
 
-	public final org.springframework.data.jpa.domain.QAbstractPersistable _super = new org.springframework.data.jpa.domain.QAbstractPersistable(
-			this);
+    public final org.springframework.data.jpa.domain.QAbstractPersistable _super = new org.springframework.data.jpa.domain.QAbstractPersistable(this);
 
-	public final StringPath city = createString("city");
+    public final StringPath city = createString("city");
 
-	public final StringPath country = createString("country");
+    public final StringPath country = createString("country");
 
-	public final NumberPath<Long> id = createNumber("id", Long.class);
+    public final NumberPath<Long> id = createNumber("id", Long.class);
 
-	public final StringPath postalCode = createString("postalCode");
+    public final StringPath postalCode = createString("postalCode");
 
-	public final StringPath street = createString("street");
+    public final StringPath street = createString("street");
 
-	public final EnumPath<Address.Type> type = createEnum("type", Address.Type.class);
+    public final EnumPath<Address.Type> type = createEnum("type", Address.Type.class);
 
-	public final QUser user;
+    public final QUser user;
 
-	public QAddress(String variable) {
-		this(Address.class, forVariable(variable), INITS);
-	}
+    public QAddress(String variable) {
+        this(Address.class, forVariable(variable), INITS);
+    }
 
-	public QAddress(PathMetadata<?> metadata) {
-		this(metadata, metadata.isRoot() ? INITS : PathInits.DEFAULT);
-	}
+    public QAddress(PathMetadata<?> metadata) {
+        this(metadata, metadata.isRoot() ? INITS : PathInits.DEFAULT);
+    }
 
-	public QAddress(PathMetadata<?> metadata, PathInits inits) {
-		this(Address.class, metadata, inits);
-	}
+    public QAddress(PathMetadata<?> metadata, PathInits inits) {
+        this(Address.class, metadata, inits);
+    }
 
-	public QAddress(Class<? extends Address> type, PathMetadata<?> metadata, PathInits inits) {
-		super(type, metadata, inits);
-		this.user = inits.isInitialized("user") ? new QUser(forProperty("user")) : null;
-	}
+    public QAddress(Class<? extends Address> type, PathMetadata<?> metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.user = inits.isInitialized("user") ? new QUser(forProperty("user")) : null;
+    }
 
 }
+
