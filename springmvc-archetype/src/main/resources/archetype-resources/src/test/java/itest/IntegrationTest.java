588f7b7f5ba43e07b54c39d7f27ef6d8fc93ad7c
Ralph Schaer <ralphschaer@gmail.com>
2012-02-26 14:18:04 +0100

    fix package name

--- springmvc-archetype/src/main/resources/archetype-resources/src/test/java/itest/IntegrationTest.java
+++ springmvc-archetype/src/main/resources/archetype-resources/src/test/java/itest/IntegrationTest.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 
 import org.junit.Test;
 
-import ${groupId}.embeddedtc.TomcatTest;
+import ch.ralscha.embeddedtc.TomcatTest;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
--- webappsimple-archetype/src/main/resources/archetype-resources/src/test/java/itest/IntegrationTest.java
+++ webappsimple-archetype/src/main/resources/archetype-resources/src/test/java/itest/IntegrationTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import ${groupId}.embeddedtc.TomcatTest;
+import ch.ralscha.embeddedtc.TomcatTest;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
