09815293f34c369b74529a2e9ba397c156fac348
Ralph Schaer <ralphschaer@gmail.com>
2012-05-25 07:59:58 +0200

    removed unused import

--- eds-starter-simple-app/src/main/java/ch/rasc/eds/starter/config/SpringConfig.java
+++ eds-starter-simple-app/src/main/java/ch/rasc/eds/starter/config/SpringConfig.java
@@ -3,7 +3,6 @@ package ch.rasc.eds.starter.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
