79113dac091dbab188745a190c2055cc7bec3390
Ralph Schaer <ralphschaer@gmail.com>
2012-06-05 08:18:31 +0200

    added exception handler for form_post methods

---
+++ eds-starter-simple-app/src/main/java/ch/rasc/eds/starter/service/FormPostExceptionHandler.java
@@ -0,0 +1,28 @@
+package ch.rasc.eds.starter.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import ch.ralscha.extdirectspring.bean.ExtDirectResponseBuilder;
+
+@Component
+@Lazy
+public class FormPostExceptionHandler implements HandlerExceptionResolver {
+
+	@Override
+	public ModelAndView resolveException(final HttpServletRequest request, final HttpServletResponse response,
+			final Object handler, final Exception ex) {
+
+		LoggerFactory.getLogger(FormPostExceptionHandler.class).error("error", ex);
+		ExtDirectResponseBuilder.create(request, response).setException(ex).buildAndWrite();
+		return null;
+
+	}
+
+}
