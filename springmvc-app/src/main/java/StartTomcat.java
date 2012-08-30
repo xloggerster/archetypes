7c5dbf18c8e53d6c5442e6fee16a508aa5934b99
Ralph Schaer <ralphschaer@gmail.com>
2012-08-30 21:19:52 +0200

    Simplify StartTomcat class

--- springmvc-app/src/main/java/StartTomcat.java
+++ springmvc-app/src/main/java/StartTomcat.java
@@ -2,7 +2,6 @@ import ch.rasc.embeddedtc.EmbeddedTomcat;
 
 public class StartTomcat {
 	public static void main(String[] args) throws Exception {
-		EmbeddedTomcat et = new EmbeddedTomcat();
-		et.startAndWait();
+		EmbeddedTomcat.create().startAndWait();
 	}
 }
