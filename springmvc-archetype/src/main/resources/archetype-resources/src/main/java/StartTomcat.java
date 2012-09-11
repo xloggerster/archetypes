3cbcd45c56ae24bd84bc20d655cd2709ae88215f
Ralph Schaer <ralphschaer@gmail.com>
2012-09-11 08:51:07 +0200

    Use builder style to start Tomcat

--- springmvc-archetype/src/main/resources/archetype-resources/src/main/java/StartTomcat.java
+++ springmvc-archetype/src/main/resources/archetype-resources/src/main/java/StartTomcat.java
@@ -5,7 +5,6 @@ import ch.rasc.embeddedtc.EmbeddedTomcat;
 
 public class StartTomcat {
 	public static void main(String[] args) throws Exception {
-		EmbeddedTomcat et = new EmbeddedTomcat();
-		et.startAndWait();
+		EmbeddedTomcat.create().startAndWait();
 	}
 }
