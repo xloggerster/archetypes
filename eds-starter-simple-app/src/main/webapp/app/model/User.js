b510d4a4ed496aa655866fc2151f1090fcc3b42e
Ralph Schaer <ralphschaer@gmail.com>
2013-03-01 10:10:13 +0100

    Generated model

--- eds-starter-simple-app/src/main/webapp/app/model/User.js
+++ eds-starter-simple-app/src/main/webapp/app/model/User.js
@@ -1,40 +1,43 @@
-Ext.define('Starter.model.User', {
-	extend: 'Ext.data.Model',
-	requires: [ 'Ext.data.UuidGenerator'],
-	idgen: 'uuid',
-
-	fields: [ {
-		name: 'lastName',
-		type: 'string'
-	}, {
-		name: 'firstName',
-		type: 'string'
-	}, {
-		name: 'email',
-		type: 'string'
-	}, {
-		name: 'department',
-		type: 'string'
-	} ],
-
-	validations: [ {
-		type: 'email',
-		field: 'email'
-	}, {
-		type: 'presence',
-		field: 'lastName'
-	} ],
-
-	proxy: {
-		type: 'direct',
-		api: {
-			read: storeService.read,
-			create: storeService.create,
-			update: storeService.update,
-			destroy: storeService.destroy
-		},
-		reader: {
-			root: 'records'
-		}
-	}
+Ext.define("Starter.model.User",
+{
+  extend : "Ext.data.Model",
+  fields : [ {
+    name : "id",
+    type : "string"
+  }, {
+    name : "firstName",
+    type : "string",
+    convert : null
+  }, {
+    name : "lastName",
+    type : "string",
+    convert : null
+  }, {
+    name : "email",
+    type : "string",
+    convert : null
+  }, {
+    name : "department",
+    type : "string",
+    convert : null
+  } ],
+  validations : [ {
+    type : "presence",
+    field : "lastName"
+  }, {
+    type : "email",
+    field : "email"
+  } ],
+  proxy : {
+    type : "direct",
+    api : {
+      read : storeService.read,
+      create : storeService.create,
+      update : storeService.update,
+      destroy : storeService.destroy
+    },
+    reader : {
+      root : "records"
+    }
+  }
 });
